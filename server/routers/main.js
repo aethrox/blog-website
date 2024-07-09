521e6c95e02cfc393d3a14a76d61768c2886a823
aethrox <69115453+aethrox@users.noreply.github.com>
2024-07-09 17:51:20 +0300

    Update main.js

--- server/routers/main.js
+++ server/routers/main.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 // Routes
-router.get('/', (req, res) => {
-  res.send("Hello World!");
+router.get('', (req, res) => { // Ana sayfa route'u oluşturduk (GET)
+  const locals = {
+    title: "NodeJS Blog",
+    description: "Simple blog created with NodeJS, Express & MongoDB"
+  }
+  res.render('index', { locals });
+  // index.ejs dosyası, layout/main.ejs dosyasını kullanarak render edilecek
+  // locals objesi, index.ejs dosyasına gönderilen verileri içerir
+  // { locals } şeklinde gönderilen veriler, index.ejs dosyasında locals objesi olarak kullanılabilir
 });
 
-module.exports = router;
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+module.exports = router; // Router'ı dışarıya aktardık
