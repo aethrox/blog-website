2054b54db6000eb7ae18c8d22d3d6e9c5f4333c7
aethrox <69115453+aethrox@users.noreply.github.com>
2024-07-10 15:43:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require('dotenv').config(); // Burada .env dosyasını yükledik
 const express = require('express'); // Express modülünü yükledik
 const expressLayouts = require('express-ejs-layouts'); // Express Layouts modülünü yükledik
 
+const connectDB = require('./server/config/db'); // Veritabanı bağlantısını yaptık
+
 const app = express(); // Express uygulamasını başlattık
 const PORT = 5000 || process.env.PORT; // Port numarasını belirledik (5000 veya .env dosyasındaki PORT)
+
+// Connect to MongoDB
+connectDB(); // MongoDB'ye bağlandık
 
 app.use(express.static('public')); // Public dosyasını statik olarak belirledik
 
