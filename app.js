f002f003300a1488acd4573cb0f7b7e097b09168
aethrox <69115453+aethrox@users.noreply.github.com>
2024-07-12 14:07:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express');
 // Express, Node.js web uygulamaları oluşturmak için kullanılan bir web uygulama çerçevesidir.
 const expressLayouts = require('express-ejs-layouts');
 // Express Layouts, Express uygulamalarında layout'ları kullanmak için kullanılır.
+const methodOverride = require('method-override');
+// Method Override, HTTP metodlarını değiştirmek için kullanılır.
+// Örneğin; DELETE ve PUT metodlarını kullanmak için kullanılır.
+// DELETE ve PUT metodları, HTML form'larında desteklenmez.
 const cookieParser = require('cookie-parser'); 
 // cookie-parser, cookie'leri parse etmek için kullanılır.
 const session = require('express-session');
@@ -14,6 +18,7 @@ const MongoStore = require('connect-mongo');
 
 
 const connectDB = require('./server/config/db'); // Veritabanı bağlantısını yaptık
+const { isActiveRoute } = require('./server/helpers/routeHelpers'); // isActiveRoute fonksiyonunu import ettik
 
 const app = express(); // Express uygulamasını başlattık
 const PORT = 5000 || process.env.PORT; // Port numarasını belirledik (5000 veya .env dosyasındaki PORT)
@@ -27,6 +32,8 @@ app.use(express.json());
  // JSON verilerini almak için gerekli middleware (req.body)
 app.use(cookieParser());
 // Cookie'leri parse etmek için gerekli middleware (req.cookies)
+app.use(methodOverride('_method'));
+// HTTP metodlarını değiştirmek için gerekli middleware (req.method)
 
 app.use(session({
   secret: 'keyboard cat',
@@ -48,6 +55,12 @@ app.use(expressLayouts); // Express Layouts modülünü kullanacağımızı beli
 app.set('layout', './layouts/main'); // Layout dosyasını belirledik
 // Layout dosyası, genel olarak tüm sayfalarda kullanılacak olan HTML yapısını belirler
 app.set('view engine', 'ejs'); // View Engine olarak EJS'i belirledik
+
+app.locals.isActiveRoute = isActiveRoute; // isActiveRoute fonksiyonunu global olarak tanımladık
+// Bu sayede tüm view dosyalarında bu fonksiyonu kullanabiliriz
+// Örneğin; <li class="<%= isActiveRoute('/', request.path) %>">...</li>
+// Bu kod, anasayfa route'u aktif olduğunda class'ı "active" yapar
+// app.locals objesi, tüm view dosyalarında kullanılabilir ve global değişkenler tanımlamak için kullanılır.
 
 app.use('/', require('./server/routers/main')); // Main router'ı kullanacağımızı belirttik
 app.use('/', require('./server/routers/admin')); // Main router'ı kullanacağımızı belirttik
