48722a01d87b1dadb32f721cf923bbb7a02c3487
aethrox <69115453+aethrox@users.noreply.github.com>
2024-07-12 14:07:41 +0300

    Update admin.js

--- server/routers/admin.js
+++ server/routers/admin.js
@@ -10,9 +10,10 @@ const jwt = require('jsonwebtoken');
 const adminLayout = '../views/layouts/admin'; // Admin layout dosyasının yolu
 const jwtSecret = process.env.JWT_SECRET; // JWT secret key'i .env dosyasından alındı
 
-// Check Login Route
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = (req, res, next) => { // Auth middleware oluşturduk
+  // Bu middleware, kullanıcının token'ı olup olmadığını kontrol eder
+  // Eğer token varsa, kullanıcının id'sini request objesine ekler
   const token = req.cookies.token;
 
   if (!token) {
@@ -20,9 +21,9 @@ const authMiddleware = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, jwtSecret);
-    req.userId = decoded.userId;
-    next();
+    const decoded = jwt.verify(token, jwtSecret); // Token'ı verify ettik ve decoded objesine atadık
+    req.userId = decoded.userId; // Request objesine userId'yi ekledik ve sonraki middleware'larda kullanacağız
+    next(); // Sonraki middleware'e geçmek için next() fonksiyonunu çağırdık
   } catch (error) {
     res.status(401).json({ message: 'Unauthorized' });
   }
@@ -74,7 +75,80 @@ router.post('/admin', async (req, res) => {
 });
 
 router.get('/dashboard', authMiddleware, async (req, res) => {
-  res.render('admin/dashboard');
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "Simple blog created with NodeJS, Express & MongoDB"
+    }
+    
+    const data = await Post.find().sort({ createdAt: -1 })
+    res.render('admin/dashboard', { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
+router.get('/add-post', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Add Post",
+      description: "Simple blog created with NodeJS, Express & MongoDB"
+    }
+    
+    const data = await Post.find();
+    res.render('admin/add-post', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
+router.post('/add-post', authMiddleware, async (req, res) => {
+  try {
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body,
+      })
+
+      await Post.create(newPost);
+      res.redirect('/dashboard');
+
+    } catch (error) {
+      console.log("Error: " + error.message);
+    }
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    const locals = {
+      title: "Edit Post",
+      description: "Simple blog created with NodeJS, Express & MongoDB"
+    }
+
+    const data = await Post.findOne({ _id: req.params.id });
+
+    res.render('admin/edit-post', { data, locals, layout: adminLayout });
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now()
+    });
+
+    res.redirect(`/edit-post/${req.params.id}`);
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
 });
 
 // Admin - Register Route
@@ -100,6 +174,25 @@ router.post('/register', async (req, res) => { // Ana sayfa route'u oluşturduk 
   } catch (error) {
     console.log("Error: " + error.message);
   }
+});
+
+// Delete Post Route
+
+router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
+// Logout Route
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('token');
+  // res.json({ message: 'Logged out successful!' });
+  res.redirect('/');
 });
 
 
